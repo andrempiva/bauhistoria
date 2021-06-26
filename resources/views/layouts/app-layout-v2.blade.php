35b38a3af724076cd371e54bdf26b058dafe331d
André M Piva <andrempiva@gmail.com>
2021-06-26 15:12:41 -0300

    middleware + header template bugfix

--- resources/views/layouts/app-layout-v2.blade.php
+++ resources/views/layouts/app-layout-v2.blade.php
@@ -31,7 +31,8 @@
         <main class="container mx-auto pt-4 min-h-full">
             <!-- Page Content -->
             <div class="bg-white border b-gray-400 shadow">
-                <div class="card-header bg-gray-100 border-b border-gray-200 pt-4 px-4">
+                <div class="card-header bg-gray-100 border-b
+                border-gray-200 px-4 py-2 font-bold text-gray-700">
                     {{ $header }}
                 </div>
                 <div class="card-body p-4">
--- resources/views/story/index.blade.php
+++ resources/views/story/index.blade.php
@@ -1,13 +1,12 @@
 <x-app-layout-v2>
     <x-slot name="title">Histórias</x-slot>
-    <x-slot name="header">
-    </x-slot>
+    <x-slot name="header">Histórias</x-slot>
     <x-slot name="slot">
-        <div class="flex justify-between mb-2">
+        {{-- <div class="flex justify-between mb-2">
             <h3 class="font-serif text-4xl text-gray-700">
                 {{ __('Stories') }}
             </h3>
-        </div>
+        </div> --}}
         <div class="flex items-center mb-4">
             <a href="{{ route('story.create') }}"
                 class="px-4 py-2 bg-gray-800 border
